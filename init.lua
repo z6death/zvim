840426a352d2ffb8a2964d6b12cc5fac033618b4
z6death <tnz426.z6@gmail.com>
2025-06-11 16:26:40 +0600

    inital

--- init.lua
+++ init.lua
@@ -8,6 +8,23 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.wo.number = true
 
+require('lualine').setup {
+  options = {
+    theme = 'auto',  
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+}
+
+ 
 local ls = require("luasnip")
 
 map({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
@@ -37,8 +54,39 @@ map('n', '<leader>l', '<C-w>l', opts)
 map('n', '<C-s>', ':SessionSave<CR>', opts)
 map('n', '<C-o>', ':Telescope session-lens search_session<CR>', opts)
 
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.completion.spell,
+        require("none-ls.diagnostics.eslint") 
+    }
+})
+
+local npairs = require("nvim-autopairs")
+local Rule = require('nvim-autopairs.rule')
+
+npairs.setup({
+    check_ts = true,
+    ts_config = {
+        lua = {'string'},
+        javascript = {'template_string'},
+        java = false,
+    }
+})
+
+local ts_conds = require('nvim-autopairs.ts-conds')
+
+npairs.add_rules({
+  Rule("%", "%", "lua")
+    :with_pair(ts_conds.is_ts_node({'string','comment'})),
+  Rule("$", "$", "lua")
+    :with_pair(ts_conds.is_not_ts_node({'function'}))
+})
+
 vim.opt.expandtab = true     
 vim.opt.shiftwidth = 4      
 vim.opt.tabstop = 4        
 vim.opt.softtabstop = 4 
+
 
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,6 @@
+  return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+  }
