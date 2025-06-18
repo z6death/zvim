a0b77c82cd8190beb3bce9a2660bf3a7fd8308c8
z6death <tnz426.z6@gmail.com>
2025-06-18 17:55:35 +0600

    initial

--- init.lua
+++ init.lua
@@ -4,6 +4,23 @@ vim.cmd("colorscheme zet")
 vim.opt.clipboard = "unnamedplus"
 vim.o.laststatus = 3
 
+vim.opt.expandtab = true     
+vim.opt.shiftwidth = 4      
+vim.opt.tabstop = 4        
+vim.opt.softtabstop = 4
+
+require("telescope").load_extension("session-lens")
+
+require("telescope").setup({
+  extensions = {
+    ["session-lens"] = {
+      path_display = { "shorten" },
+      cwd_only = false,
+      session_dir = "/home/z6/.config/nvim/sessions",
+    },
+  },
+})
+
 
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
@@ -14,7 +31,6 @@ vim.wo.number = true
 require('lualine').setup {
   options = {
     theme = 'zet',  
---    globalstatus = true,
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
   },
@@ -27,7 +43,6 @@ require('lualine').setup {
     lualine_z = {'location'},
   },
 }
-
  
 local ls = require("luasnip")
 
@@ -42,10 +57,10 @@ map({"i", "s"}, "<C-E>", function()
 end, {silent = true})
 
 local builtin = require('telescope.builtin')
-map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>ff', builtin.find_files, { desc = 'find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'find word' })
 map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+map('n', '<leader>fh', builtin.oldfiles, { desc = 'recently opened files' })
 
 map('n', '<leader>n', ':Neotree toggle<CR>', opts)
 map('n', '<leader>c', ':Neotree focus<CR>', opts)
@@ -88,9 +103,80 @@ npairs.add_rules({
     :with_pair(ts_conds.is_not_ts_node({'function'}))
 })
 
-vim.opt.expandtab = true     
-vim.opt.shiftwidth = 4      
-vim.opt.tabstop = 4        
-vim.opt.softtabstop = 4 
+map('n', '<leader>ss', '<cmd>SessionSave<CR>', { desc = 'save <s>ession' })
+map('n', '<leader>so', '<cmd>SessionRestore<CR>', { desc = 'session <o>pen' })
+map('n', '<leader>sd', '<cmd>SessionDelete<CR>', { desc = 'session <d>elete' })
+map('n', '<leader>sq', '<cmd>qa<CR>', { desc = 'session <q>uit' })
+map('n', '<leader>sm', '<cmd>Telescope session-lens search_session<CR>', { desc = 'Browse Sessions' })
 
+map('n', '<leader>sl', function()
+  local session_dir = vim.fn.stdpath("config") .. "/sessions/"
+  local sessions = vim.fn.glob(session_dir .. "*.vim", false, true)
+  table.sort(sessions, function(a, b)
+    return vim.fn.getftime(a) > vim.fn.getftime(b)
+  end)
+  if #sessions > 0 then
+    vim.cmd("source " .. sessions[1])
+    print("Loaded session: " .. vim.fn.fnamemodify(sessions[1], ":t"))
+  else
+    print("No sessions found.")
+  end
+end, { desc = '[S]ession [L]ast' })
+
+require("auto-session").setup({
+  session_dir = "/home/z6/.config/nvim/sessions", 
+  auto_session_enabled = true,
+  auto_save_enabled = true,
+  auto_restore_enabled = true,
+  auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+})
+
+_G.load_last_session = function()
+  local session_dir = "/home/z6/nvim/sessions/"
+  local sessions = vim.fn.glob(session_dir .. "*.vim", false, true)
+
+  table.sort(sessions, function(a, b)
+    return vim.fn.getftime(a) > vim.fn.getftime(b)
+  end)
+
+  if #sessions > 0 then
+    vim.cmd("source " .. sessions[1])
+    vim.notify("Loaded session: " .. vim.fn.fnamemodify(sessions[1], ":t"), vim.log.levels.INFO)
+  else
+    vim.notify("No sessions found.", vim.log.levels.WARN)
+  end
+end
+
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = {
+		"                        ███                 ",
+		"                       ░░░                  ",
+		" █████████ █████ █████ ████  █████████████  ",
+		"░█░░░░███ ░░███ ░░███ ░░███ ░░███░░███░░███ ",
+		"░   ███░   ░███  ░███  ░███  ░███ ░███ ░███ ",
+		"  ███░   █ ░░███ ███   ░███  ░███ ░███ ░███ ",
+		 " █████████  ░░█████    █████ █████░███ █████",
+		"░░░░░░░░░    ░░░░░    ░░░░░ ░░░░░ ░░░ ░░░░░ ",
+        "                                            ",
+        "                                            ",
+}
+
+dashboard.section.buttons.val = {
+  dashboard.button("Space  e", "  New file", ":ene <BAR> startinsert<CR>"),
+  dashboard.button("Space ff", "󰈞  Find file", ":Telescope find_files<CR>"),
+  dashboard.button("Space fh", "󰊄  Recently opened files", ":Telescope oldfiles<CR>"),
+  dashboard.button("Space fg", "󰈬  Find word", ":Telescope live_grep<CR>"),
+  dashboard.button("Space sm", "  Browse Sessions", ":Telescope session-lens search_session<CR>"),
+  dashboard.button("Space sl", "  Open last session", ":lua load_last_session()<CR>")
+}
+
+dashboard.config.layout = {
+  dashboard.section.header,
+  dashboard.section.buttons,
+  dashboard.section.footer,
+}
+
+alpha.setup(dashboard.config)
 
--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,12 +1,21 @@
 return {
-  'rmagatti/auto-session',
-  lazy = false,
-
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
-  }
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      log_level = "error",
+      auto_session_enable_last_session = false,
+      auto_session_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
+      auto_session_use_git_branch = false,
+      session_lens = {
+        load_on_setup = false,
+      },
+      auto_session_suppress_dirs = {
+        "~/", "~/Downloads", "/",
+      },
+      session_dir = vim.fn.stdpath("config") .. "/sessions/",
+    })
+  end,
 }
+
